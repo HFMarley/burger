8d949cc99dd9fae6c740d9cdbfb0fbaae2030025
HFMarley <hankfoxmarley@gmail.com>
2019-01-27 20:45:11 -0600

    added multiple files and code

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-var path = require("path");
+
 var express = require("express");
 
-var app = express();
 var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
 
 //expresslibrary allows us to use middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_Controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-})
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
